daaec02be4eee05ca118035ec0c155437eb7fded
ajie <1651588782@qq.com>
2023-01-25 22:03:00 +0800

    MyBatis基本使用

---
+++ Module01_MyBatis/Mybatis_start/src/main/java/com/ajie/pojo/User.java
@@ -0,0 +1,14 @@
+package com.ajie.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: ajie
+ * @Date: 2023/1/25
+ * @Description: 测试实体类
+ */
+@Data
+public class User {
+    private Integer id;
+    private String name;
+}
---
+++ Module01_MyBatis/Mybatis_start/src/main/java/com/ajie/test/MyTest.java
@@ -0,0 +1,42 @@
+package com.ajie.test;
+
+import com.ajie.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @Author: ajie
+ * @Date: 2023/1/25
+ * @Description:
+ */
+public class MyTest {
+
+    @Test
+    public void selectAll() throws IOException {
+        //加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+
+        //获取sqlSession 工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        //获取 sqlSession 对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行 sql 语句
+        List<User> users = sqlSession.selectList("userMapper.findAll");
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        //关闭资源
+        sqlSession.close();
+    }
+}
