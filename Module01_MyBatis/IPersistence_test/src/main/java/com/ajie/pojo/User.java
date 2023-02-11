bc17f75057eb6d5c3029ede9e8b94c58b48a35c2
ajie <1651588782@qq.com>
2023-02-11 23:25:39 +0800

    JDBC 问题的分析 + 自定义持久层框架使用端的编写: sqlMapConfig.xml 和 mapper.xml 配置文件的编写

---
+++ Module01_MyBatis/IPersistence_test/src/main/java/com/ajie/pojo/User.java
@@ -0,0 +1,14 @@
+package com.ajie.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: ajie
+ * @Date: 2023/2/11
+ * @Description:
+ */
+@Data
+public class User {
+    private Integer id;
+    private String username;
+}
